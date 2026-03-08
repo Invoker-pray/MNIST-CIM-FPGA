048241c212cb18d370edc8d95fc5a72d72d2e766
Invoker-pray <jiaohongbao04@gmail.com>
2026-03-08 11:08:04 +0800

    Sun Mar  8 11:08:04 AM CST 2026

--- hw/scripts/csrc/filelist.cu
+++ hw/scripts/csrc/filelist.cu
@@ -1,20 +1,20 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _8769_archive_1.so
-_8769_archive_1.so : archive.22/_8769_archive_1.a
+ARCHIVE_OBJS += _11237_archive_1.so
+_11237_archive_1.so : archive.23/_11237_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../../sim/fc1_to_fc2_top_simv.daidir//_8769_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../../sim/mnist_inference_top_simv.daidir//_11237_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../../sim/fc1_to_fc2_top_simv.daidir//_8769_archive_1.so $@
+	@ln -sf .//../../sim/mnist_inference_top_simv.daidir//_11237_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.22/_prev_archive_1.a
+_prev_archive_1.so : archive.23/_prev_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic  -o .//../../sim/fc1_to_fc2_top_simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic  -o .//../../sim/mnist_inference_top_simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../../sim/fc1_to_fc2_top_simv.daidir//_prev_archive_1.so $@
+	@ln -sf .//../../sim/mnist_inference_top_simv.daidir//_prev_archive_1.so $@
 
 
 
